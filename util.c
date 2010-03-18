12c39869ffed8635c5a94a84bd4b1eb931cffb66
Dave Reisner <d@falconindy.com>
2010-03-18 16:34:17 -0400

    Add colorize() function

--- util.c
+++ util.c
@@ -1,2 +1,9 @@
+#include <stdio.h>
+#include <string.h>
 
+#include "util.h"
 
+char *colorize(const char* input, int color, char* buffer) {
+    sprintf(buffer, "\033[1;3%dm%s\033[1;m\0", color, input);
+    return buffer;
+}
--- util.h
+++ util.h
@@ -1,6 +1,5 @@
-#include <string.h>
 
-/* Color constants. Add 9 for bold */
+/* Color constants. Add for bold */
 #define BLACK   0
 #define RED     1
 #define GREEN   2
@@ -10,5 +9,6 @@
 #define CYAN    6
 #define WHITE   7
 #define FG      8
+#define BOLD    9
 
-char *colorize(char*, int color);
+char *colorize(const char*, int, char*);
