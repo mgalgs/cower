22e8a1f3b49bb87de8f89192ff0bf9393434085e
Dave Reisner <d@falconindy.com>
2010-03-18 14:24:05 -0400

    Track new file: cower.h

---
+++ cower.h
@@ -0,0 +1,18 @@
+#ifndef _COWER_H
+#define _COWER_H
+
+/* Operations */
+enum {
+    OPER_SEARCH = 1;
+    OPER_UPDATE = 2;
+    OPER_INFO = 4;
+    OPER_DOWNLOAD = 8;
+};
+
+/* Options */
+enum {
+    OPT_COLOR = 1;
+    OPT_VERBOSE = 2;
+};
+
+#endif /* _COWER_H */
