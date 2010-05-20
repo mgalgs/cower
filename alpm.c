c8effa9b83f8d1e83342d3895569860d44177093
Dave Reisner <d@falconindy.com>
2010-05-19 23:31:13 -0400

    alpm: rename alpmutil->alpm

--- alpm.c
+++ alpm.c
@@ -1,5 +1,5 @@
 /*
- *  alpmutil.c
+ *  alpm.c
  *
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
@@ -19,7 +19,7 @@
 #include <stdio.h>
 #include <string.h>
 
-#include "alpmutil.h"
+#include "alpm.h"
 #include "conf.h"
 #include "util.h"
 
