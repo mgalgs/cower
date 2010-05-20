b94b245765205787f65305f1b71483c0fc48479d
Dave Reisner <d@falconindy.com>
2010-05-19 23:14:36 -0400

    curl: add curl specific header and source file

---
+++ curl.c
@@ -0,0 +1,40 @@
+/*
+ *  curl.c
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "conf.h"
+#include "curl.h"
+
+int curl_local_init() {
+  curl = curl_easy_init();
+
+  if (! curl)
+    return 1;
+
+  if (config->verbose > 1) {
+    printf("::DEBUG:: Initializing curl\n");
+    curl_easy_setopt(curl, CURLOPT_VERBOSE, 1);
+  }
+
+  curl_easy_setopt(curl, CURLOPT_ENCODING, "deflate, gzip");
+
+  return 0;
+}
+
---
+++ curl.h
@@ -0,0 +1,29 @@
+/*
+ *  curl.h
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef _COWER_CURL_H
+#define _COWER_CURL_H
+
+#include <curl/curl.h>
+#include <curl/easy.h>
+
+int curl_local_init(void);
+
+CURL *curl;
+extern CURL *curl;
+
+#endif /* _COWER_CURL_H */
