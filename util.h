373a7b7bf86519e93ecec6c8128dc90ba18bd70b
Dave Reisner <d@falconindy.com>
2010-05-20 11:30:35 -0400

    util: include <stdio.h> not "stdio.h"

--- util.h
+++ util.h
@@ -17,9 +17,10 @@
 #ifndef _COWER_UTIL_H
 #define _COWER_UTIL_H
 
+#include <stdio.h>
+
 #include "pacman.h"
 #include "package.h"
-#include "stdio.h"
 
 /* constants for cfprintf */
 #define C_ON      "\033[1;3"
