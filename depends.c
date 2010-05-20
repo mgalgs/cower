517a5a87314a608b0952f13c936847008a74e54d
Dave Reisner <d@falconindy.com>
2010-05-20 12:31:56 -0400

    depends: include linux/limits header

--- depends.c
+++ depends.c
@@ -15,12 +15,14 @@
  *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+#include <linux/limits.h>
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
 #include <sys/stat.h>
 #include <unistd.h>
 
+#include "aur.h"
 #include "conf.h"
 #include "depends.h"
 #include "download.h"
