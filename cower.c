8b708051f48718bb4da75c6fd10bb6ccc6102c5f
Dave Reisner <d@falconindy.com>
2010-05-22 13:46:13 -0400

    conf: implement color config parsing
    read from config file at $XDG_CONFIG_HOME/cower/cower.conf
    declare static structure of valid colors and corresponding values
    use binary search to validate color options in config file

--- cower.c
+++ cower.c
@@ -29,7 +29,43 @@
 #include "update.h"
 #include "util.h"
 
+#define NUM_COLORS (sizeof(availcolors)/sizeof(availcolors[0]))
+
 static alpm_list_t *targets; /* Package argument list */
+
+static struct color_t {
+  const char *name;
+  unsigned short val;
+} availcolors[] = {
+  { "BLACK",             BLACK }, { "BLUE",               BLUE },
+  { "BOLDBLACK",     BOLDBLACK }, { "BOLDBLUE",       BOLDBLUE },
+  { "BOLDCYAN",       BOLDCYAN }, { "BOLDFG",           BOLDFG },
+  { "BOLDGREEN",     BOLDGREEN }, { "BOLDMAGENTA", BOLDMAGENTA },
+  { "BOLDRED",         BOLDRED }, { "BOLDWHITE",     BOLDWHITE },
+  { "BOLDYELLOW",   BOLDYELLOW }, { "CYAN",               CYAN },
+  { "FG",                   FG }, { "GREEN",             GREEN },
+  { "MAGENTA",         MAGENTA }, { "RED",                 RED },
+  { "WHITE",             WHITE }, { "YELLOW",           YELLOW }
+};
+
+static int fn_cmp_color(const void *c1, const void *c2) {
+  struct color_t *color1 = (struct color_t*)c1;
+  struct color_t *color2 = (struct color_t*)c2;
+
+  return strcmp(color1->name, color2->name);
+}
+
+static unsigned short color_is_valid(const char *colorname) {
+  struct color_t key, *result;
+  key.name = colorname;
+
+  result = bsearch(&key, availcolors, NUM_COLORS, sizeof(struct color_t), fn_cmp_color);
+
+  if (result != NULL)
+    return ((struct color_t*)result)->val;
+  else
+    return 0;
+}
 
 static void cleanup(int ret) {
   curl_easy_cleanup(curl);
@@ -116,6 +152,77 @@ static int parseargs(int argc, char **argv) {
   return 0;
 }
 
+static int read_config_file() {
+  int ret = 0;
+  char *ptr;
+  char config_path[PATH_MAX + 1], line[BUFSIZ + 1];
+
+  /* XXX: What happens if XDG_CONFIG_HOME isn't defined? */
+
+  snprintf(&config_path[0], PATH_MAX, "%s/%s",
+    getenv("XDG_CONFIG_HOME"), "cower/cower.conf");
+
+  if (!file_exists(config_path)) {
+    /* Return without error. Nothing bad happened, we
+     * just don't have a config file to look at.
+     */
+    return ret;
+  }
+
+  FILE *conf_fd = fopen(config_path, "r");
+  while (fgets(line, BUFSIZ, conf_fd)) {
+    strtrim(line);
+
+    if (line[0] == '#' || strlen(line) == 0)
+      continue;
+
+    if ((ptr = strchr(line, '#')))
+      *ptr = '\0';
+
+    char *key;
+    unsigned short color;
+    key = ptr = line;
+    strsep(&ptr, "=");
+    strtrim(key);
+    strtrim(ptr);
+
+    /* TODO: Don't let invalid colors go silently */
+    if (STREQ(key, "repo")) {
+      if ((color = color_is_valid(ptr)) != 0)
+        config->colors->repo = color;
+    } else if (STREQ(key, "packages")) {
+      if ((color = color_is_valid(ptr)) != 0)
+        config->colors->pkg = color;
+    } else if (STREQ(key, "uptodate")) {
+      if ((color = color_is_valid(ptr)) != 0)
+        config->colors->uptodate = color;
+    } else if (STREQ(key, "outofdate")) {
+      if ((color = color_is_valid(ptr)) != 0)
+        config->colors->outofdate = color;
+    } else if (STREQ(key, "url")) {
+      if ((color = color_is_valid(ptr)) != 0)
+        config->colors->url = color;
+    } else if (STREQ(key, "info")) {
+      if ((color = color_is_valid(ptr)) != 0)
+        config->colors->info = color;
+    } else if (STREQ(key, "warn")) {
+      if ((color = color_is_valid(ptr)) != 0)
+        config->colors->warn = color;
+    } else if (STREQ(key, "error")) {
+      if ((color = color_is_valid(ptr)) != 0)
+        config->colors->error = color;
+    } else {
+      fprintf(stderr, "Error parsing config file: bad option '%s'\n", key);
+      ret = 1;
+      break;
+    }
+  }
+
+  fclose(conf_fd);
+
+  return ret;
+}
+
 static void usage() {
 printf("cower %s\n\
 Usage: cower [options] <operation> PACKAGE [PACKAGE2..]\n\
@@ -156,7 +263,9 @@ int main(int argc, char **argv) {
   }
 
   if (config->color) {
-    /* TODO: Read config file for custom colors if it exists */
+    ret = read_config_file();
+    if (ret > 0)
+      cleanup(ret);
   }
 
   curl_global_init(CURL_GLOBAL_NOTHING);
