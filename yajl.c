0e1b0efe2654f4a892cf7d77c9263773cc64b838
Dave Reisner <d@falconindy.com>
2010-05-23 18:53:17 -0400

    yajl: use the ctx parameter of the callback the way it was intended.

--- yajl.c
+++ yajl.c
@@ -14,67 +14,74 @@
 static yajl_handle hand;
 static alpm_list_t *pkg_list;
 
-static struct yajl_parse_struct {
+struct yajl_parse_struct {
   struct aur_pkg_t *aurpkg;
   char curkey[32];
   int json_depth;
-} parse_struct;
+};
 
 static int json_string(void *ctx, const unsigned char *data, unsigned int size) {
+  struct yajl_parse_struct *parse_struct = (struct yajl_parse_struct*)ctx;
   const char *val = (const char*)data;
 
-  if(STREQ(parse_struct.curkey, AUR_QUERY_TYPE) && 
+  if(STREQ(parse_struct->curkey, AUR_QUERY_TYPE) &&
      strncmp(val, AUR_QUERY_ERROR, strlen(AUR_QUERY_ERROR)) == 0) {
     return 0;
   }
 
-  if (STREQ(parse_struct.curkey, AUR_ID))
-    parse_struct.aurpkg->id = strndup(val, size);
-  else if (STREQ(parse_struct.curkey, AUR_NAME))
-    parse_struct.aurpkg->name = strndup(val, size);
-  else if (STREQ(parse_struct.curkey, AUR_VER))
-    parse_struct.aurpkg->ver = strndup(val, size);
-  else if (STREQ(parse_struct.curkey, AUR_CAT))
-    parse_struct.aurpkg->cat = strndup(val, size);
-  else if (STREQ(parse_struct.curkey, AUR_DESC))
-    parse_struct.aurpkg->desc = strndup(val, size);
-  else if (STREQ(parse_struct.curkey, AUR_URL))
-    parse_struct.aurpkg->url = strndup(val, size);
-  else if (STREQ(parse_struct.curkey, AUR_URLPATH))
-    parse_struct.aurpkg->urlpath = strndup(val, size);
-  else if (STREQ(parse_struct.curkey, AUR_LICENSE))
-    parse_struct.aurpkg->lic = strndup(val, size);
-  else if (STREQ(parse_struct.curkey, AUR_VOTES))
-    parse_struct.aurpkg->votes = strndup(val, size);
-  else if (STREQ(parse_struct.curkey, AUR_OOD))
-    parse_struct.aurpkg->ood = strncmp(val, "1", 1) == 0 ? 1 : 0;
+  if (STREQ(parse_struct->curkey, AUR_ID))
+    parse_struct->aurpkg->id = strndup(val, size);
+  else if (STREQ(parse_struct->curkey, AUR_NAME))
+    parse_struct->aurpkg->name = strndup(val, size);
+  else if (STREQ(parse_struct->curkey, AUR_VER))
+    parse_struct->aurpkg->ver = strndup(val, size);
+  else if (STREQ(parse_struct->curkey, AUR_CAT))
+    parse_struct->aurpkg->cat = strndup(val, size);
+  else if (STREQ(parse_struct->curkey, AUR_DESC))
+    parse_struct->aurpkg->desc = strndup(val, size);
+  else if (STREQ(parse_struct->curkey, AUR_URL))
+    parse_struct->aurpkg->url = strndup(val, size);
+  else if (STREQ(parse_struct->curkey, AUR_URLPATH))
+    parse_struct->aurpkg->urlpath = strndup(val, size);
+  else if (STREQ(parse_struct->curkey, AUR_LICENSE))
+    parse_struct->aurpkg->lic = strndup(val, size);
+  else if (STREQ(parse_struct->curkey, AUR_VOTES))
+    parse_struct->aurpkg->votes = strndup(val, size);
+  else if (STREQ(parse_struct->curkey, AUR_OOD))
+    parse_struct->aurpkg->ood = strncmp(val, "1", 1) == 0 ? 1 : 0;
 
   return 1;
 }
 
 static int json_map_key(void *ctx, const unsigned char *data, unsigned int size) {
-  strncpy(parse_struct.curkey, (const char*)data, size);
-  parse_struct.curkey[size] = '\0';
+  struct yajl_parse_struct *parse_struct = (struct yajl_parse_struct*)ctx;
+
+  strncpy(parse_struct->curkey, (const char*)data, size);
+  parse_struct->curkey[size] = '\0';
 
   return 1;
 }
 
 static int json_start_map(void *ctx) {
-  if (parse_struct.json_depth++ >= 1)
-    parse_struct.aurpkg = aur_pkg_new();
+  struct yajl_parse_struct *parse_struct = (struct yajl_parse_struct*)ctx;
+
+  if (parse_struct->json_depth++ >= 1)
+    parse_struct->aurpkg = aur_pkg_new();
 
   return 1;
 }
 
 
 static int json_end_map(void *ctx) {
-  if (! --(parse_struct.json_depth)) {
-    aur_pkg_free(parse_struct.aurpkg);
+  struct yajl_parse_struct *parse_struct = (struct yajl_parse_struct*)ctx;
+
+  if (! --(parse_struct->json_depth)) {
+    aur_pkg_free(parse_struct->aurpkg);
     return 0;
   }
 
-  pkg_list = alpm_list_add_sorted(pkg_list, parse_struct.aurpkg, aur_pkg_cmp);
-  parse_struct.aurpkg = NULL;
+  pkg_list = alpm_list_add_sorted(pkg_list, parse_struct->aurpkg, aur_pkg_cmp);
+  parse_struct->aurpkg = NULL;
 
   return 1;
 }
@@ -100,22 +107,20 @@ static size_t curl_write_yajl(void *ptr, size_t size, size_t nmemb, void *stream
 }
 
 alpm_list_t *aur_fetch_json(const char *url) {
-  yajl_gen g;
-  /* yajl_status yajlstat; */
   CURLcode curlstat;
   long httpcode;
+  struct yajl_parse_struct *parse_struct;
 
-  parse_struct.aurpkg = NULL;
-  parse_struct.json_depth = 0;
   pkg_list = NULL;
+  parse_struct = malloc(sizeof *parse_struct);
+  parse_struct->aurpkg = NULL;
+  parse_struct->json_depth = 0;
 
   curl_easy_setopt(curl, CURLOPT_URL, url);
   curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, curl_write_yajl);
   curl_easy_setopt(curl, CURLOPT_WRITEDATA, NULL);
 
-  g = yajl_gen_alloc(NULL, NULL);
-
-  hand = yajl_alloc(&callbacks, NULL, NULL, (void*)g);
+  hand = yajl_alloc(&callbacks, NULL, NULL, (void*)parse_struct);
 
   curlstat = curl_easy_perform(curl);
   if (curlstat != CURLE_OK) {
@@ -129,11 +134,10 @@ alpm_list_t *aur_fetch_json(const char *url) {
     goto cleanup;
   }
 
-  aur_pkg_free(parse_struct.aurpkg);
-
 cleanup:
+  aur_pkg_free(parse_struct->aurpkg);
+  free(parse_struct);
   yajl_parse_complete(hand);
-  yajl_gen_free(g);
   yajl_free(hand);
 
   return pkg_list;
