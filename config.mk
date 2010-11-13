0f8494f5674478e2d2ce3006b5626de9db6a6be0
Dave Reisner <d@falconindy.com>
2010-11-13 08:38:54 -0500

    remove debugging build flags

--- config.mk
+++ config.mk
@@ -6,8 +6,8 @@ PREFIX ?= /usr/local
 MANPREFIX ?= ${PREFIX}/share/man
 
 # compiler flags
-CC = c99 -pipe
-CPPFLAGS = -DCOWER_VERSION=\"${VERSION}\" -D_FORTIFY_SOURCE=2 ${PMCHECK}
+CC = c99
+CPPFLAGS = -DCOWER_VERSION=\"${VERSION}\" ${PMCHECK}
 CFLAGS += -g -pedantic -Wall -Wextra ${CPPFLAGS}
 LDFLAGS += -lcurl -lalpm -lyajl -larchive -pthread
 
