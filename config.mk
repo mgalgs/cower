37a716e55a60a96f05591131cb17e86c038ad2c2
Dave Reisner <d@falconindy.com>
2011-01-02 09:28:20 -0500

    allow overriding of the compiler. push --std=c99 into CFLAGS

--- config.mk
+++ config.mk
@@ -6,8 +6,8 @@ PREFIX ?= /usr/local
 MANPREFIX ?= ${PREFIX}/share/man
 
 # compiler flags
-CC = c99
+CC ?= gcc
 CPPFLAGS = -DCOWER_VERSION=\"${VERSION}\" ${PMCHECK}
-CFLAGS += -g -pedantic -Wall -Wextra ${CPPFLAGS}
+CFLAGS += --std=c99 -g -pedantic -Wall -Wextra ${CPPFLAGS}
 LDFLAGS += -lcurl -lalpm -lyajl -larchive -pthread
 
