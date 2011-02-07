689344d5e994193d45618e632d9170017f5864dd
Dave Reisner <d@falconindy.com>
2011-02-07 08:50:45 -0500

    compile with -Werror

--- config.mk
+++ config.mk
@@ -8,6 +8,6 @@ MANPREFIX ?= ${PREFIX}/share/man
 # compiler flags
 CC ?= gcc
 CPPFLAGS = -DCOWER_VERSION=\"${VERSION}\" ${PMCHECK}
-CFLAGS += --std=c99 -g -pedantic -Wall -Wextra ${CPPFLAGS}
+CFLAGS += --std=c99 -g -pedantic -Wall -Wextra -Werror ${CPPFLAGS}
 LDFLAGS += -lcurl -lalpm -lyajl -larchive -pthread
 
